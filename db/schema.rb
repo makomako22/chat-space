9376a8671296dc1535f3b5cd6cb59afa8402c3f0
荒木真 <lineinfo@infoseek.jp>
2017-02-24 18:44:05 +0900

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224083513) do
+ActiveRecord::Schema.define(version: 20170224093825) do
+
+  create_table "gruop_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "gruop_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "gruops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
