f5213ca5b1c4fef13070d7e5d739de80368ccafc
荒木真 <lineinfo@infoseek.jp>
2017-02-24 18:01:41 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'chats#index'
-  resources :chats
+  root      'chats#index'
+  resources :chats, only: :index
+  resources :gruops, only: [:show, :edit]
 end
