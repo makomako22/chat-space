91c39915aec187c24942c1b963757cd3b67fc555
荒木真 <lineinfo@infoseek.jp>
2017-02-26 09:09:33 +0900

    Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root      'chats#index'
   resources :chats, only: :index
-  resources :groups, only: [:show, :edit]
+  resources :groups, only: [:new, :edit]
 end
