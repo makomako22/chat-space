02eb879ee2f22ed45910d191e0e3e54d20fff33a
荒木真 <lineinfo@infoseek.jp>
2017-03-10 16:02:41 +0900

    Edit groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,6 +14,21 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:id])
+    @users = User.all
+  end
+
+  def update
+    group = Group.find(params[:id])
+    if group.update(group_params)
+      redirect_to root_path, notice: "チャットグループが更新されました"
+    else
+      flash[:alert] = "nameを入力してください"
+      render :new
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, user_ids:[])
