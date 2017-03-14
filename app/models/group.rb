188aed9f37a37691120b4759e5f400ebb3472380
荒木真 <lineinfo@infoseek.jp>
2017-03-14 16:22:17 +0900

    Edit models/group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :chats
   has_many :group_users
   has_many :users, through: :group_users
+
+  def latest_chat
+    if chats.present?
+      chats.last.text
+    else
+      "まだメッセージはありません。"
+    end
+  end
 end
