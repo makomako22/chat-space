6fd86731318bd016ca2b79493a387bceef409c68
荒木真 <lineinfo@infoseek.jp>
2017-03-20 21:41:10 +0900

    Edit chats_controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,5 @@
 class ChatsController < ApplicationController
-  before_action :find_group, :set_groups, :set_chat, :set_group_chats, only: :index
+  before_action :set_group, :set_groups, :set_chat, :set_group_chats, only: :index
 
   def index
   end
@@ -15,7 +15,7 @@ class ChatsController < ApplicationController
     params.require(:chat).permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 
-  def find_group
+  def set_group
     @group = Group.find(params[:group_id])
   end
 
