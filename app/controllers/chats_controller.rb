6bc51242e4d9d36121fa29391c9f63301e2d37f7
荒木真 <lineinfo@infoseek.jp>
2017-03-20 21:57:06 +0900

    Edit chats_controller.rb

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,18 +1,21 @@
 class ChatsController < ApplicationController
-  before_action :set_group, :set_groups, :set_chat, :set_group_chats, only: :index
+  before_action :set_group, only: [:index, :create]
+  before_action :set_groups, only: :index
+  before_action :set_chat, only: :index
+  before_action :set_group_chats, only: :index
 
   def index
   end
 
   def create
-    @chat = Chat.new(chat_params)
+    @chat = @group.chats.new(chat_params)
     @chat.save
     redirect_to root_path, notice: "メッセージを作成できました。"
   end
 
   private
   def chat_params
-    params.require(:chat).permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
+    params.require(:chat).permit(:text).merge(user_id: current_user.id)
   end
 
   def set_group
