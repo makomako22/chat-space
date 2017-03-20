0b5dfeb365ad4ffc019f571f4b5c49ee72d00c27
荒木真 <lineinfo@infoseek.jp>
2017-03-20 18:26:08 +0900

    Edit chat.rb

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,13 @@
 class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  def name
+    user.name
+  end
+
+  def date
+    created_at.strftime("%Y-%m-%d %H:%M:%S")
+  end
+
 end
