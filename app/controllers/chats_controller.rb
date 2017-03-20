c9152cff9e6e28972e7744c19df41584e09fe309
荒木真 <lineinfo@infoseek.jp>
2017-03-20 18:23:28 +0900

    rename_method

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,5 @@
 class ChatsController < ApplicationController
-  before_action :set_groups, :set_chats, only: :index
+  before_action :find_group, :set_groups, :set_chat, :set_group_chats, only: :index
 
   def index
   end
@@ -15,13 +15,15 @@ class ChatsController < ApplicationController
     params.require(:chat).permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 
-  def set_groups
+  def find_group
     @group = Group.find(params[:group_id])
-    @groups = current_user.groups
   end
 
-  def set_chats
+  def set_chat
     @chat = Chat.new
+  end
+
+  def set_group_chats
     @chats = @group.chats
   end
 
