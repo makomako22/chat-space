7f657b7256b9b43cdc4142d83851b5cf43fe05e8
荒木真 <lineinfo@infoseek.jp>
2017-03-23 19:17:30 +0900

    Edit rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,10 @@
 ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path("spec/support/controller_macros.rb")
 require 'rspec/rails'
+require 'devise'
+require 'rails-controller-testing'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -18,7 +21,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -28,11 +31,17 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryGirl::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+    [:controller, :view, :request].each do |type|
+     config.include ::Rails::Controller::Testing::TestProcess, :type => type
+     config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
+     config.include ::Rails::Controller::Testing::Integration, :type => type
+    end
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
