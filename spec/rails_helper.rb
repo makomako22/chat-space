c7174b9fd3666866970fbdf63f5f555ac45d5476
荒木真 <lineinfo@infoseek.jp>
2017-03-24 17:21:01 +0900

    Edit rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,7 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
