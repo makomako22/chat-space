265a43946f7fde5a3c8ba8be1bdf836b5309f806
荒木真 <lineinfo@infoseek.jp>
2017-03-24 22:10:29 +0900

    Edit chats_controller_spec.rb

--- spec/controllers/chats_controller_spec.rb
+++ spec/controllers/chats_controller_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe ChatsController, type: :controller do
   let(:user) { create(:user) }
   let(:group) { create(:group) }
   let(:chat) { create(:chat) }
+  let(:chats) { create(:group.chats) }
+  let(:groups) { create(:user.groups) }
 
   before do
     login_user
@@ -27,18 +29,30 @@ RSpec.describe ChatsController, type: :controller do
       expect(assigns(:chat)).to be_a_new(Chat)
     end
 
+    it 'assigns the requested chat to @chats' do
+      expect(assigns(:chats)).to eq group.chats
+    end
+
+    it 'assigns the requested chat to @groups' do
+      expect(assigns(:groups)).to eq user.groups
+    end
+
   end
 
   describe 'POST #create' do
+    let(:make) { post :create, params: { group_id: group, chat: attributes_for(:chat, group_id: group.id, user_id: user.id) } }
 
     it 'save a new chat' do
-      expect(chat.save).to be true
+      expect{ make }.to change(Chat, :count).by(1)
     end
 
-    it 'dose not save a new chat without body' do
-      chat = build(:chat, text: nil)
-      chat.valid?
-      expect(chat.errors[:text]).to include('を入力してください。')
+    it 'dose not save a new chat without text' do
+      expect{ make }.not_to change(Chat, :count)
+    end
+
+    it 'redirect_to root_path' do
+      make
+      expect(response).to redirect_to group_chats_path(group)
     end
 
   end
