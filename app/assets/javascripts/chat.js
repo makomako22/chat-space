44b1527ff9e20bd8bb262a0a61524384bad48dc3
荒木真 <lineinfo@infoseek.jp>
2017-04-01 13:50:31 +0900

    Edit chat.js

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,46 +1,47 @@
-$(function() {
+$(document).on('turbolinks:load', function(){
+
   function buildHTML(chat) {
     var html =
      `<div class="chat__box">
         <div class="chat__box--name">
-         chat.user.name
-         </div>
+         ${chat.name}
+        </div>
         <div class="chat__box--date">
-          chat.date
+          ${chat.date}
         </div>
         <div class="chat__box--text">
-          chat.text
+          ${chat.text}
         </div>
       </div>`;
     return html;
   }
 
-  $('.message').on('submit', function(e) {
+  $('.chat_form').on('submit', function(e) {
     e.preventDefault();
     console.log("確認");
-    var textField = $('.message__box--text');
+    var textField = $('#chat_text');
     console.log(textField);
-    var formData = new FormData($('.message').get(0));
-    console.log(formData);
+    var formdata = new FormData($(this).get(0));
+    console.log(formdata);
     $.ajax({
       type: 'POST',
       url: './chats',
-      data: formData,
+      data: formdata,
       dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data) {
-      console.log("ここまでの確認");
+    $('.chat').delay(100).animate({
+      scrollTop: $(document).height()
+    },1500);
       var html = buildHTML(data);
-      console.log(html);
-      $('.chat__box').append(html);
+      $('.chat').append(html);
       textField.val('');
     })
     .fail(function() {
       console.log("エラーメッセージ");
       alert('error');
     });
-    return false;
   });
 });
