24aa122eed1bd9e7c272f8ca462e50f336d8963d
荒木真 <lineinfo@infoseek.jp>
2017-04-07 15:16:49 +0900

    Edit chat.rb

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,6 +2,7 @@ class Chat < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   belongs_to :group
+  mount_uploader :image, ImageUploader
 
   def date
     created_at.strftime("%Y-%m-%d %H:%M:%S")
