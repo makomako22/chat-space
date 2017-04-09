9642afd752e3e258e96a09922a5cb07b825a05ff
荒木真 <lineinfo@infoseek.jp>
2017-04-09 19:15:53 +0900

    Add validates

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,11 +1,15 @@
 class Chat < ApplicationRecord
-  validates :text, presence: true
+  validates :text_or_image, presence: true
   belongs_to :user
   belongs_to :group
   mount_uploader :image, ImageUploader
 
   def date
     created_at.strftime("%Y-%m-%d %H:%M:%S")
+  end
+
+  def text_or_image
+    text.presence or image.presence
   end
 
 end
